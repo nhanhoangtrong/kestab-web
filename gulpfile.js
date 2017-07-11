d8f7fb711ef69f28a84d9e7a01d0a4bbcc8a6669
Nhan Hoang <siege250294@gmail.com>
2017-07-11 17:55:57 +0700

    js files: append semicolons in the end of javascript commands

--- gulpfile.js
+++ gulpfile.js
@@ -9,18 +9,18 @@ const 	gulp = require('gulp'),
 		stylus = require('gulp-stylus'),
 		hb = require('gulp-hb'),
 		rename = require('gulp-rename'),
-		replace = require('gulp-replace')
+		replace = require('gulp-replace');
 
 const logPluginError = function(pluginName) {
 	return function(error) {
-		gutil.log(pluginName, error.message)
-		this.emit('end')
-	}
-}
+		gutil.log(pluginName, error.message);
+		this.emit('end');
+	};
+};
 
-const runSequence = require('run-sequence')
-const del = require('del')
-const browserSync = require('browser-sync').create()
+const runSequence = require('run-sequence');
+const del = require('del');
+const browserSync = require('browser-sync').create();
 
 const src = {
 	styl: './src/styl/**/*.styl',
@@ -33,111 +33,111 @@ const src = {
 	layouts: './src/templates/*.hbs',
 	partials: './src/templates/partials/*.hbs',
 	data: './src/templates/data/*.{json,js}',
-	helpers: './src/templates/helpers/*.js'
-}
+	helpers: './src/templates/helpers/*.js',
+};
 
 const dest = {
 	styl: './src/css',
-	html: './src'
-}
+	html: './src',
+};
 
 const dist = {
 	css: './dist/css',
 	js: './dist/js',
 	html: './dist/',
-	img: './dist/img'
-}
+	img: './dist/img',
+};
 
 gulp.task('stylus', function() {
 	return gulp.src(src.styl)
 		.pipe(stylus().on('error', logPluginError('stylus')))
 		.pipe(gulp.dest(dest.styl))
 		.pipe(browserSync.stream({
-			once: true
-		}))
-})
+			once: true,
+		}));
+});
 
 gulp.task('handlebars', function() {
 	return gulp.src(src.layouts)
 		.pipe(hb({
 			partials: src.partials,
 			data: src.data,
-			helpers: src.helpers
+			helpers: src.helpers,
 		}).on('error', logPluginError('handlebars')))
 		.pipe(rename(function(path) {
-			path.extname = '.html'
+			path.extname = '.html';
 		}))
 		.pipe(gulp.dest(dest.html))
 		.pipe(browserSync.reload({
-			stream: true
-		}))
-})
+			stream: true,
+		}));
+});
 
 gulp.task('browserSync', function() {
-	const baseDir = process.env.NODE_ENV === 'production' ? './dist' : './src'
+	const baseDir = process.env.NODE_ENV === 'production' ? './dist' : './src';
 	browserSync.init({
 		server: {
-			baseDir: baseDir
-		}
-	})
-})
+			baseDir: baseDir,
+		},
+	});
+});
 
 gulp.task('htmlmin', function() {
 	return gulp.src(src.html)
 		.pipe(useref())
 		.pipe(htmlmin({
-			collapseWhitespace: true
+			collapseWhitespace: true,
 		}).on('error', logPluginError('htmlmin')))
-		.pipe(gulp.dest(dist.html))
-})
+		.pipe(gulp.dest(dist.html));
+});
 
 gulp.task('uglify', function() {
 	return gulp.src(src.js)
 		.pipe(uglify().on('error', logPluginError('uglify')))
-		.pipe(gulp.dest(dist.js))
-})
+		.pipe(gulp.dest(dist.js));
+});
 
 gulp.task('cssnano', function() {
 	return gulp.src(src.css)
 		.pipe(cssnano().on('error', logPluginError('cssnano')))
-		.pipe(gulp.dest(dist.css))
-})
+		.pipe(gulp.dest(dist.css));
+});
 
 gulp.task('imagemin', function() {
 	return gulp.src(src.img)
 		.pipe(imagemin().on('error', logPluginError('imagemin')))
-		.pipe(gulp.dest(dist.img))
-})
+		.pipe(gulp.dest(dist.img));
+});
 
 gulp.task('watch', ['stylus', 'browserSync', 'handlebars'], function() {
 	watch(src.styl, function(vinyl) {
-		gulp.start('stylus')
-	})
+		gulp.start('stylus');
+	});
 	watch(src.templates, function(vinyl) {
-		gulp.start('handlebars')
-	})
-	watch(src.js, browserSync.reload)
-	watch(src.img, browserSync.reload)
-})
+		gulp.start('handlebars');
+	});
+	watch(src.js, browserSync.reload);
+	watch(src.img, browserSync.reload);
+});
 
 gulp.task('serve', function(cb) {
-	runSequence('build', 'browserSync', cb)
-})
+	runSequence('build', 'browserSync', cb);
+});
 
 gulp.task('clean', function(cb) {
 	del('./dist').then(function() {
-		cb()
-	})
-})
+		cb();
+	});
+});
 
 gulp.task('build', function(cb) {
-	runSequence('clean', ['stylus', 'handlebars'], ['cssnano', 'uglify', 'htmlmin', 'imagemin'], cb)
-})
+	runSequence('clean', ['stylus', 'handlebars'], ['cssnano', 'uglify', 'htmlmin', 'imagemin'], cb);
+});
 
 gulp.task('start', function(cb) {
 	if (process.env.NODE_ENV === 'production') {
-		gulp.start('serve')
+		gulp.start('serve');
 	} else {
-		gulp.start('watch')
+		gulp.start('watch');
 	}
-})
+});
--- src/templates/helpers/ifeq.js
+++ src/templates/helpers/ifeq.js
@@ -1,4 +1,4 @@
 module.exports = function(value1, value2, options) {
-	console.log(value1, value2)
-	return value1 === value2 ? options.fn(this) : options.inverse(this)
-}
+	console.log(value1, value2);
+	return value1 === value2 ? options.fn(this) : options.inverse(this);
+};
